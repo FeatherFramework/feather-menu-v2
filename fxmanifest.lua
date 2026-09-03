adcc80f9ea7af5bcb82833893796bd0b3b95a40d
David Fountain <davfount@gmail.com>
2026-09-03 13:58:35 -0700

    Version Bump

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -5,7 +5,7 @@ lua54 'yes'
 
 author 'Feather Framework'
 description 'A focused, reactive menu system for Feather Framework and standalone RedM resources'
-version '2.0.0-alpha.1'
+version '2.0.0-alpha.2'
 
 ui_page 'ui/index.html'
 
